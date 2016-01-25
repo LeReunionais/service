18d789c35d060ad9a5be15aa0b6264b4003dfc7e
LeReunionais <reunionaisle@gmail.com>
2016-01-25 21:30:14 +0800

    go fmt Use jsonrpc 2.0 format

--- service.go
+++ service.go
@@ -1,62 +1,73 @@
 package service
 
 import (
-  "log"
-  "encoding/json"
-  zmq "github.com/pebbe/zmq4"
-  "github.com/LeReunionais/ip"
+	"encoding/json"
+	"github.com/LeReunionais/ip"
+	zmq "github.com/pebbe/zmq4"
+	"github.com/satori/go.uuid"
+	"log"
 )
 
 type message struct {
-  Action string `json:"action"`
-  Service Service`json:"service"`
+	Jsonrpc string `json:"jsonrpc"`
+	ID      string `json:"id"`
+	Command string `json:"command"`
+	Params  params `json:"params"`
 }
 
+type params struct {
+	Service Service `json:"service"`
+}
+
+// Service represents a service that we want to register. It should hold all information need to be able to be used by another service
 type Service struct {
-  Name string `json:"name"`
-  Hostname string `json:"hostname"`
-  Protocol string `json:"protocol"`
-  Port int `json:"port"`
+	Name     string `json:"name"`
+	Hostname string `json:"hostname"`
+	Protocol string `json:"protocol"`
+	Port     int    `json:"port"`
 }
 
-func RegisterAllInterfaces (endpoint, name, protocol string, port int) []string {
-  ips := ip.Ips()
-  for _, ip := range ips {
-    service := Service{
-      Name: name,
-      Hostname: ip,
-      Protocol: protocol,
-      Port: port,
-    }
-    Register(endpoint, service)
-  }
-  return ips
+// RegisterAllInterfaces will automatically register host interfaces
+func RegisterAllInterfaces(endpoint, name, protocol string, port int) []string {
+	ips := ip.Ips()
+	for _, ip := range ips {
+		service := Service{
+			Name:     name,
+			Hostname: ip,
+			Protocol: protocol,
+			Port:     port,
+		}
+		Register(endpoint, service)
+	}
+	return ips
 }
 
-func Register (endpoint string, service Service) {
-  pusher, errSock := zmq.NewSocket(zmq.PUSH)
-  defer pusher.Close()
-  if errSock != nil {
-    log.Fatal(errSock)
-  }
-  log.Println("pusher created")
+// Register will register one service
+func Register(endpoint string, service Service) {
+	pusher, errSock := zmq.NewSocket(zmq.PUSH)
+	defer pusher.Close()
+	if errSock != nil {
+		log.Fatal(errSock)
+	}
+	log.Println("pusher created")
+	errCon := pusher.Connect(endpoint)
+	if errCon != nil {
+		log.Fatal(errCon)
+	}
+	log.Println("pusher connected to", endpoint)
 
-  errCon := pusher.Connect(endpoint)
-  if errCon != nil {
-    log.Fatal(errCon)
-  }
-  log.Println("pusher connected to", endpoint)
+	registerMsg := message{
+		Jsonrpc: "2.0",
+		ID:      uuid.NewV4().String(),
+		Command: "register",
+		Params:  params{service},
+	}
+	registerMsgByt, errMar := json.Marshal(registerMsg)
+	if errMar != nil {
+		log.Fatal(registerMsg)
+		log.Fatal(errMar)
+	}
 
-  registerMsg := message{
-    Action: "register",
-    Service: service,
-  }
-  registerMsgByt,errMar := json.Marshal(registerMsg)
-  if errMar != nil {
-    log.Fatal(registerMsg)
-    log.Fatal(errMar)
-  }
-
-  pusher.Send(string(registerMsgByt), 0)
-  log.Println("pusher sent some message ", string(registerMsgByt))
+	pusher.Send(string(registerMsgByt), 0)
+	log.Println("pusher sent some message ", string(registerMsgByt))
 }
--- service_test.go
+++ service_test.go
@@ -1,72 +1,81 @@
 package service
 
 import (
-  "testing"
-  "fmt"
-  zmq "github.com/pebbe/zmq4"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
+	"regexp"
+	"testing"
 )
 
 func TestRegisterAllInterfaces(t *testing.T) {
-    // Socket to receive message
-    receiver, _ := zmq.NewSocket(zmq.PULL)
-    defer receiver.Close()
-    receiver.Bind("tcp://127.0.0.1:3001")
+	// Socket to receive message
+	receiver, _ := zmq.NewSocket(zmq.PULL)
+	defer receiver.Close()
+	error := receiver.Bind("tcp://127.0.0.1:3001")
+	if error != nil {
+		t.Fatal(error)
+	}
 
-    endpoint := "tcp://127.0.0.1:3001"
+	endpoint := "tcp://127.0.0.1:3001"
 
-    cases := []struct {
-      nameIn string
-      protocolIn string
-      portIn int
-      want string
-    }{
-      { "youpi", "HTTP", 80, `{"action":"register","service":{"name":"youpi","hostname":"localhost","protocol":"HTTP","port":80}}`},
-    }
+	cases := []struct {
+		nameIn     string
+		protocolIn string
+		portIn     int
+		want       string
+	}{
+		{"youpi", "HTTP", 80, `{"action":"register","service":{"name":"youpi","hostname":"localhost","protocol":"HTTP","port":80}}`},
+	}
 
-    for _, c := range cases {
-      registeredIps := RegisterAllInterfaces(endpoint, c.nameIn, c.protocolIn, c.portIn)
-      var registrations []string
-      for _ = range registeredIps {
-        got,_ := receiver.Recv(0)
-        fmt.Println("Received message", got)
-        registrations = append(registrations, got)
-      }
-      if len(registeredIps) != len(registrations) {
-        t.Errorf("Didn't receive expected number of registrations. Expected: %d, received: %d", len(registeredIps), len(registrations))
-      }
-    }
+	for _, c := range cases {
+		registeredIps := RegisterAllInterfaces(endpoint, c.nameIn, c.protocolIn, c.portIn)
+		var registrations []string
+		for _ = range registeredIps {
+			got, _ := receiver.Recv(0)
+			fmt.Println("Received message", got)
+			registrations = append(registrations, got)
+		}
+		if len(registeredIps) != len(registrations) {
+			t.Errorf("Didn't receive expected number of registrations. Expected: %d, received: %d", len(registeredIps), len(registrations))
+		}
+	}
 }
 
 func TestRegister(t *testing.T) {
-    // Socket to receive message
-    receiver, _ := zmq.NewSocket(zmq.PULL)
-    defer receiver.Close()
-    receiver.Bind("tcp://127.0.0.1:3001")
+	// Socket to receive message
+	receiver, _ := zmq.NewSocket(zmq.PULL)
+	defer receiver.Close()
+	error := receiver.Bind("tcp://127.0.0.1:3002")
+	if error != nil {
+		t.Fatal(error)
+	}
 
-    endpoint := "tcp://127.0.0.1:3001"
+	endpoint := "tcp://127.0.0.1:3002"
 
-    service := Service{
-      Name: "youpi",
-      Hostname: "localhost",
-      Protocol: "HTTP",
-      Port: 80,
-    }
-    cases := []struct {
-      in Service
-      want string
-    }{
-      { service, `{"action":"register","service":{"name":"youpi","hostname":"localhost","protocol":"HTTP","port":80}}`},
-    }
+	service := Service{
+		Name:     "youpi",
+		Hostname: "localhost",
+		Protocol: "HTTP",
+		Port:     80,
+	}
+	cases := []struct {
+		in   Service
+		want string
+	}{
+		{service, `{"jsonrpc":"2.0","id":".*","command":"register","params":{"service":{"name":"youpi","hostname":"localhost","protocol":"HTTP","port":80}}}`},
+	}
 
-    for _, c := range cases {
-      Register(endpoint, c.in)
+	for _, c := range cases {
+		go Register(endpoint, c.in)
 
-      got,_ := receiver.Recv(0)
-      fmt.Println("Received message", got)
-      if got != c.want {
-        inString,_ := json.Marshal(c.in)
-        t.Errorf("Register(%q, %s) sent %s, expected %s", endpoint, inString, got, c.want)
-      }
-    }
+		fmt.Println("Waiting for something to be received")
+		got, _ := receiver.Recv(0)
+		fmt.Println("Received message", got)
+		matched, _ := regexp.MatchString(c.want, got)
+		if !matched {
+			inString, _ := json.Marshal(c.in)
+			t.Errorf("Register(%q, %s) sent %s, expected %s", endpoint, inString, got, c.want)
+		}
+	}
 }
