71154c298828d74db66959b19b97e9b48e1be19a
LeReunionais <reunionaisle@gmail.com>
2016-01-28 23:55:49 +0800

    Fix #2

--- common.go
+++ common.go
@@ -1,14 +1,18 @@
 package service
 
 type message struct {
-	Jsonrpc string `json:"jsonrpc"`
-	ID      string `json:"id"`
-	Method 	string `json:"method"`
-	Params  params `json:"params"`
+	Jsonrpc string      `json:"jsonrpc"`
+	ID      string      `json:"id"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
 }
 
 type params struct {
 	Service Service `json:"service"`
+}
+
+type findparams struct {
+	Name string `json:"name"`
 }
 
 // Service represents a service that we want to register. It should hold all information need to be able to be used by another service
@@ -17,4 +21,10 @@ type Service struct {
 	Hostname string `json:"hostname"`
 	Protocol string `json:"protocol"`
 	Port     int    `json:"port"`
+}
+
+type findreply struct {
+	ID      string  `json:"id"`
+	Jsonrpc string  `json:"jsonrpc"`
+	Result  Service `json:"result"`
 }
--- whereis.go
+++ whereis.go
@@ -1,15 +1,51 @@
 package service
 
 import (
-  "log"
+	"encoding/json"
+	zmq "github.com/pebbe/zmq4"
+	"github.com/satori/go.uuid"
+	"log"
 )
 
 func Whereis(endpoint, name string) Service {
-  log.Println("Where is", name,"?")
-  return Service{
-    "youpi",
-    "192.168.0.104",
-    "REP",
-    5000,
-  }
+	log.Println("Where is", name, "?")
+	requester, errSock := zmq.NewSocket(zmq.REQ)
+	defer requester.Close()
+	if errSock != nil {
+		log.Fatal(errSock)
+	}
+	log.Println("requester created")
+
+	errCon := requester.Connect(endpoint)
+	if errCon != nil {
+		log.Fatal(errCon)
+	}
+	log.Println("requester connected")
+
+	whereIsMsg := message{
+		Jsonrpc: "2.0",
+		ID:      uuid.NewV4().String(),
+		Method:  "find",
+		Params:  findparams{"article"},
+	}
+	whereIsMsgByt, errMar := json.Marshal(whereIsMsg)
+	if errMar != nil {
+		log.Fatal(whereIsMsgByt)
+	}
+	requester.Send(string(whereIsMsgByt), 0)
+	log.Println("requester asked for", string(whereIsMsgByt))
+
+	reply, errRec := requester.Recv(0)
+	log.Println("received", reply)
+	if errRec != nil {
+		log.Fatal(errRec)
+	}
+
+	replyMsg := findreply{}
+	errJSON := json.Unmarshal([]byte(reply), &replyMsg)
+	if errJSON != nil {
+		log.Fatal(errJSON)
+	}
+
+	return replyMsg.Result
 }
